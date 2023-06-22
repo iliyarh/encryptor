f805d2500bfd987fd4db460aa9fb49b591fb37f3
Iliya Rahimi <83126380+iliyarh@users.noreply.github.com>
2023-06-22 22:30:25 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ from art import *
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding
+import base64
 import colorama
 import base64
 import os
+import sys
 import shutil
 import time
 import json
 import secrets
 from datetime import datetime
 import platform
+import requests
+
+version = '1.2'
 
 # Function to generate a random key
 def generate_key():
@@ -45,7 +50,7 @@ def decrypt(ciphertext, key, iv):
 # Function to print big text
 def print_big_text(text, color=colorama.Fore.WHITE):
     ascii_art = text2art(text, font='standard')
-    print(ascii_art)
+    print(color, ascii_art, colorama.Style.RESET_ALL)
 
 # Function to write encrypted text to a file
 def write_base64_file(base64_text, file_name):
@@ -119,24 +124,73 @@ def select_file():
 def clear_console():
     if platform.system() == "Windows":
         os.system("cls")
-        print_big_text("iliya RH Encryptor")
-        if os.path.exists("Data"):
-            print("The program is using existing Data.\n")
-        else:
-            print("The program couldn't find any existing Data. It will create a new one if you encrypt a text.\n")
-        
     else:
         os.system("clear")
-        print_big_text("iliya RH Encryptor")
-        if os.path.exists("Data"):
-            print("The program is using existing Data.\n")
+
+# Get The Update Version
+github_repo = "https://api.github.com/repos/iliyarh/encryptor/contents/main.py"
+response = requests.get(github_repo)
+data = response.json()
+
+# Extract the content and decode it
+content = data["content"]
+content = content.replace("\n", "")
+content = content.encode("ascii")
+content = base64.b64decode(content).decode("utf-8")
+
+# Find the value of the version variable
+version_line = next(line for line in content.splitlines() if line.startswith("version ="))
+update_version = version_line.split("=")[1].strip().strip("'").strip('"')
+
+
+def compare_files(local_file, github_file):
+    with open(local_file, "r") as file:
+        local_content = file.read()
+
+    response = requests.get(github_file)
+    github_content = response.text
+
+    if local_content == github_content:
+        print("The Program is up to date.")
+    else:
+        print("There is an update available: \U00002699 Version "+ update_version)
+        confirm_update = input("Are you sure you want to update the Program? (yes/no)")
+        if confirm_update == 'yes':
+            with open(local_file, "w") as file:
+                file.write(github_content)
+            print("Local file has been updated.")
         else:
-            print("The program couldn't find any existing Data. It will create a new one if you encrypt a text.\n")
+            print('Update has been cancelled.')
+
+# Function to update local Python file from GitHub
+def update_python_file():
+    github_repo = "https://raw.githubusercontent.com/iliyarh/encryptor/main/main.py"
+    local_file = __file__
+
+    if os.path.exists(local_file):
+        compare_files(local_file, github_repo)
+
+def restart_program():
+    python = sys.executable
+    os.execl(python, python, *sys.argv)
+
+
+# Extract the content and decode it
+content = data["content"]
+content = content.replace("\n", "")
+content = content.encode("ascii")
+content = content.decode("base64")
+
+# Find the value of the version variable
+version_line = next(line for line in content.splitlines() if line.startswith("version ="))
+version = version_line.split("=")[1].strip().strip("'").strip('"')
 
 # Initialize colorama
 colorama.init()
 
+clear_console()
 print_big_text("iliya RH Encryptor")
+print("\U00002699 Version: " + version)
 
 if os.path.exists("Data"):
     print("The program is using existing Data.\n")
@@ -145,13 +199,14 @@ else:
 
 # Main loop
 while True:
-    print(colorama.Fore.GREEN, "1. Encrypt", colorama.Style.RESET_ALL)
+    print(colorama.Fore.GREEN, "1. \U0001F510 Encrypt", colorama.Style.RESET_ALL)
     if len(os.listdir('Data/encrypted')) > 0:
-        print(colorama.Fore.RED, "2. Decrypt", colorama.Style.RESET_ALL)
+        print(colorama.Fore.RED, "2. \U0001F513 Decrypt", colorama.Style.RESET_ALL)
     if os.path.exists("Data/config.json"):
-        print(colorama.Fore.YELLOW, "3. Show Config", colorama.Style.RESET_ALL)
-        print(colorama.Fore.MAGENTA, "4. Reset Data", colorama.Style.RESET_ALL)
-    print(colorama.Fore.CYAN, "5. Clear Console", colorama.Style.RESET_ALL)
+        print(colorama.Fore.YELLOW, "3. \U0001F4DD Show Config", colorama.Style.RESET_ALL)
+        print(colorama.Fore.MAGENTA, "4. \U0001F4BE Reset Data", colorama.Style.RESET_ALL)
+    print(colorama.Fore.CYAN, "5. \U0001F6A8 Clear Console", colorama.Style.RESET_ALL)
+    print(colorama.Fore.WHITE, "6. Update Program", colorama.Style.RESET_ALL)
     print(colorama.Fore.BLUE, "0. Exit", colorama.Style.RESET_ALL)
 
     choice = input("Select an option: ")
@@ -172,15 +227,18 @@ while True:
         base64_text = base64.b64encode(ciphertext).decode('utf-8')
         write_base64_file(base64_text, f"encrypted_text_{file_name}.iliyarh")
 
-    elif choice == "2":  # Decrypt
+    elif choice == "2" and len(os.listdir('Data/encrypted')) > 0:  # Decrypt
         file_path = select_file()
         with open(file_path, "r") as file:
             ciphertext = file.read()
         key, iv = load_config()
-
+    
         ciphertext_bytes = base64.b64decode(ciphertext)
-        decrypted_text = decrypt(ciphertext_bytes, key, iv)
-        print("Decrypted text:", decrypted_text.decode('utf-8'))
+        try:
+            decrypted_text = decrypt(ciphertext_bytes, key, iv)
+            print("Decrypted text:", decrypted_text.decode('utf-8'))
+        except Exception as e:
+            print("An error occurred during decryption:", str(e))
 
     elif choice == "3":  # Show Key and IV
         key, iv = load_config()
@@ -201,6 +259,18 @@ while True:
 
     elif choice == "5":  # Clear Console
         clear_console()
+        print_big_text("iliya RH Encryptor")
+        print("\U00002699 Version: " + version)
+
+        if os.path.exists("Data"):
+            print("The program is using existing Data.\n")
+        else:
+            print("The program couldn't find any existing Data. It will create a new one if you encrypt a text.\n")
+
+
+    elif choice == "6":
+        update_python_file()
+        restart_program()
 
     elif choice == "0":  # Exit
         break
@@ -209,4 +279,4 @@ while True:
         print("Invalid choice. Please try again.\n")
 
 print_big_text("Goodbye")
-time.sleep(2)
+time.sleep(1)
